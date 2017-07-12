11f8e92645755640604c8e4e53e129c507a1c50b
forgot2015 <jouney90@163.com>
2017-07-12 14:03:02 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/ForgotCppLearning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ForgotCppLearning.dir/main.cpp.obj"
+  "ForgotCppLearning.pdb"
+  "ForgotCppLearning.exe"
+  "ForgotCppLearning.exe.manifest"
+  "libForgotCppLearning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ForgotCppLearning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
